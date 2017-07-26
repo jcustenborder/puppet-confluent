1f8a32df6b77fbbfbb59f93b9cede2e7a1f681e5
Jeremy Custenborder <jcustenborder@gmail.com>
2017-07-26 17:01:48 -0500

    Added test cases and cleanup to ensure that all log_dirs can be moved. Fixes #9 (#10)

--- spec/classes/control_center_spec.rb
+++ spec/classes/control_center_spec.rb
@@ -1,73 +1,72 @@
 require 'spec_helper'
 
 describe 'confluent::control::center' do
-  %w(RedHat Debian).each do |osfamily|
-    context "with osfamily => #{osfamily}" do
-      environment_file = nil
-
-      case osfamily
-        when 'Debian'
-          environment_file = '/etc/default/control-center'
-        when 'RedHat'
-          environment_file = '/etc/sysconfig/control-center'
-      end
-
-      let(:facts) {
-        {
-            'osfamily' => osfamily
+  supported_osfamalies.each do |osfamily, osversions|
+    osversions.each do |osversion|
+      context "with osfamily => #{osfamily} and operatingsystemmajrelease => #{osversion}" do
+        default_facts = {
+            'osfamily' => osfamily,
+            'operatingsystemmajrelease' => osversion
         }
-      }
 
-      let(:params) {
-        {
-            'config' => {
-                'bootstrap.servers' => {
-                    'value' => 'kafka-01:9092,kafka-02:9092,kafka-03:9092'
-                },
-                'confluent.controlcenter.connect.cluster' => {
-                    'value' => 'kafka-connect-01:8083,kafka-connect-02:8083,kafka-connect-03:8083'
-                },
-                'confluent.controlcenter.id' => {
-                    'value' => '1'
-                },
-                'confluent.controlcenter.streams.consumer.request.timeout.ms' => {
-                    'value' => '180000'
-                },
-                'zookeeper.connect' => {
-                    'value' => 'zookeeper-01:2181,zookeeper-02:2181,zookeeper-03:2181'
-                },
-            }
+        environment_file = nil
+
+        case osfamily
+          when 'Debian'
+            environment_file = '/etc/default/control-center'
+          when 'RedHat'
+            environment_file = '/etc/sysconfig/control-center'
+        end
+
+        let(:facts) {default_facts}
+
+        let(:params) {
+          {
+              'config' => {
+                  'bootstrap.servers' => {
+                      'value' => 'kafka-01:9092,kafka-02:9092,kafka-03:9092'
+                  },
+                  'confluent.controlcenter.connect.cluster' => {
+                      'value' => 'kafka-connect-01:8083,kafka-connect-02:8083,kafka-connect-03:8083'
+                  },
+                  'confluent.controlcenter.id' => {
+                      'value' => '1'
+                  },
+                  'confluent.controlcenter.streams.consumer.request.timeout.ms' => {
+                      'value' => '180000'
+                  },
+                  'zookeeper.connect' => {
+                      'value' => 'zookeeper-01:2181,zookeeper-02:2181,zookeeper-03:2181'
+                  },
+              }
+          }
         }
-      }
 
-      it do
         expected_heap = '-Xmx3g'
 
-        is_expected.to contain_file('/var/log/control-center')
-        is_expected.to contain_package('confluent-control-center')
-        is_expected.to contain_user('control-center')
-        is_expected.to contain_service('control-center').with(
+        it {is_expected.to contain_file('/var/log/control-center')}
+        it {is_expected.to contain_package('confluent-control-center')}
+        it {is_expected.to contain_user('control-center')}
+        it {is_expected.to contain_service('control-center').with(
             {
                 'ensure' => 'running',
                 'enable' => true
             }
-        )
+        )}
 
-        is_expected.to contain_ini_subsetting('c3_CONTROL_CENTER_HEAP_OPTS').with(
+        it {is_expected.to contain_ini_subsetting('c3_CONTROL_CENTER_HEAP_OPTS').with(
             {
                 'path' => environment_file,
                 'value' => expected_heap
             }
-        )
+        )}
 
-        is_expected.to contain_ini_setting('c3_bootstrap.servers').with(
+        it {is_expected.to contain_ini_setting('c3_bootstrap.servers').with(
             {
                 'path' => '/etc/confluent-control-center/control-center.properties',
                 'value' => 'kafka-01:9092,kafka-02:9092,kafka-03:9092'
             }
-        )
-
-
+        )}
       end
     end
   end
--- spec/classes/schema_registry_spec.rb
+++ spec/classes/schema_registry_spec.rb
@@ -1,59 +1,48 @@
 require 'spec_helper'
 
 describe 'confluent::schema::registry' do
-  %w(RedHat Debian).each do |osfamily|
-    context "with osfamily => #{osfamily}" do
-      environment_file = nil
-
-      case osfamily
-        when 'Debian'
-          environment_file = '/etc/default/schema-registry'
-        when 'RedHat'
-          environment_file = '/etc/sysconfig/schema-registry'
-      end
-
-      let(:facts) {
-        {
-            'osfamily' => osfamily
+  supported_osfamalies.each do |osfamily, osversions|
+    osversions.each do |osversion|
+      context "with osfamily => #{osfamily} and operatingsystemmajrelease => #{osversion}" do
+        default_facts = {
+            'osfamily' => osfamily,
+            'operatingsystemmajrelease' => osversion
         }
-      }
-
-      let(:params) {
-        {
+        default_params = {
             'config' => {
                 'kafkastore.connection.url' => {
                     'value' => 'zookeeper-01:2181,zookeeper-02:2181,zookeeper-03:2181'
                 }
             }
         }
-      }
 
-      it do
+        environment_file = nil
+
+        case osfamily
+          when 'Debian'
+            environment_file = '/etc/default/schema-registry'
+          when 'RedHat'
+            environment_file = '/etc/sysconfig/schema-registry'
+        end
+
+        let(:facts) {default_facts}
+        let(:params) {default_params}
+
+        log_paths = %w(/var/log/schema-registry /logs/var/lib/schema-registry)
+        log_paths.each do |log_path|
+          context "with log_path => #{log_path}" do
+            let(:params) {default_params.merge({'log_path' => log_path})}
+            it {is_expected.to contain_file(log_path).with({'owner' => 'schema-registry', 'group' => 'schema-registry'})}
+          end
+        end
+
         expected_heap = '-Xmx256M'
 
-        is_expected.to contain_ini_subsetting('schema-registry_SCHEMA_REGISTRY_HEAP_OPTS').with(
-            {
-                'path' => environment_file,
-                'value' => expected_heap
-            }
-        )
-
-        is_expected.to contain_ini_setting('schema-registry_kafkastore.connection.url').with(
-            {
-                'path' => '/etc/schema-registry/schema-registry.properties',
-                'value' => 'zookeeper-01:2181,zookeeper-02:2181,zookeeper-03:2181'
-            }
-        )
-
-        is_expected.to contain_package('confluent-schema-registry')
-        is_expected.to contain_user('schema-registry')
-        is_expected.to contain_service('schema-registry').with(
-            {
-                'ensure' => 'running',
-                'enable' => true
-            }
-        )
-        is_expected.to contain_file('/var/log/schema-registry')
+        it {is_expected.to contain_ini_subsetting('schema-registry_SCHEMA_REGISTRY_HEAP_OPTS').with({'path' => environment_file, 'value' => expected_heap})}
+        it {is_expected.to contain_ini_setting('schema-registry_kafkastore.connection.url').with({'path' => '/etc/schema-registry/schema-registry.properties', 'value' => 'zookeeper-01:2181,zookeeper-02:2181,zookeeper-03:2181'})}
+        it {is_expected.to contain_package('confluent-schema-registry')}
+        it {is_expected.to contain_user('schema-registry')}
+        it {is_expected.to contain_service('schema-registry').with({'ensure' => 'running', 'enable' => true})}
       end
     end
   end
