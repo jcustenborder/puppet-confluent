a3120ff88052e0e3b3839c84f620f2554522defe
Jeremy Custenborder <jcustenborder@gmail.com>
2017-11-16 17:40:23 -0600

    Ini_setting and Ini_subsetting are no longer used.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,5 @@ class confluent {
   Package<| tag == 'confluent' |> ->
   User<| tag == 'confluent' |> ->
   File<| tag == 'confluent' |> ->
-  Ini_setting <| tag == 'confluent' |> ->
-  Ini_subsetting <| tag == 'confluent' |> ->
   Service<| tag == 'confluent' |>
 }
