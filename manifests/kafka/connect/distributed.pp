591133272bbf2434b7c9190563d71af27473aef9
Jeremy Custenborder <jcustenborder@gmail.com>
2018-03-25 16:57:15 -0500

    Issue 71 (#73)
    * Added better default support for defaults in connect. #fixes 71.
    
    * Corrected trailing whitespace on properties.
    
    * Moved default for zookeeper_connect up to a top level variable.
    
    * Added configuration parameter property for schema-registry-urls.

--- manifests/kafka/connect/distributed.pp
+++ manifests/kafka/connect/distributed.pp
@@ -57,22 +57,33 @@
 # @param file_limit Number of file handles to configure. (SystemD only)
 class confluent::kafka::connect::distributed (
   Variant[String, Array[String]] $bootstrap_servers,
-  Hash $config                               = {},
-  Hash $environment_settings                 = {},
-  Stdlib::Unixpath $config_path              = $::confluent::params::connect_distributed_config_path,
-  Stdlib::Unixpath $logging_config_path      = $::confluent::params::connect_distributed_logging_config_path,
-  Stdlib::Unixpath $environment_path         = $::confluent::params::connect_distributed_environment_path,
-  Stdlib::Unixpath $log_path                 = $::confluent::params::connect_distributed_log_path,
-  String $user                               = $::confluent::params::connect_distributed_user,
-  String $service_name                       = $::confluent::params::connect_distributed_service,
-  Boolean $manage_service                    = $::confluent::params::connect_distributed_manage_service,
-  Enum['running', 'stopped'] $service_ensure = $::confluent::params::connect_distributed_service_ensure,
-  Boolean $service_enable                    = $::confluent::params::connect_distributed_service_enable,
-  Integer $file_limit                        = $::confluent::params::connect_distributed_file_limit,
-  Boolean $manage_repository                 = $::confluent::params::manage_repository,
-  Integer $stop_timeout_secs                 = $::confluent::params::connect_distributed_stop_timeout_secs,
-  String $heap_size                          = $::confluent::params::connect_distributed_heap_size,
-  Boolean $restart_on_logging_change         = true
+  Hash $config                                         = {},
+  Hash $environment_settings                           = {},
+  Stdlib::Unixpath $config_path                        = $::confluent::params::connect_distributed_config_path,
+  Stdlib::Unixpath $logging_config_path                = $::confluent::params::connect_distributed_logging_config_path,
+  Stdlib::Unixpath $environment_path                   = $::confluent::params::connect_distributed_environment_path,
+  Stdlib::Unixpath $log_path                           = $::confluent::params::connect_distributed_log_path,
+  String $user                                         = $::confluent::params::connect_distributed_user,
+  String $service_name                                 = $::confluent::params::connect_distributed_service,
+  Boolean $manage_service                              = $::confluent::params::connect_distributed_manage_service,
+  Enum['running', 'stopped'] $service_ensure           = $::confluent::params::connect_distributed_service_ensure,
+  Boolean $service_enable                              = $::confluent::params::connect_distributed_service_enable,
+  Integer $file_limit                                  = $::confluent::params::connect_distributed_file_limit,
+  Boolean $manage_repository                           = $::confluent::params::manage_repository,
+  Integer $stop_timeout_secs                           = $::confluent::params::connect_distributed_stop_timeout_secs,
+  String $heap_size                                    = $::confluent::params::connect_distributed_heap_size,
+  Boolean $restart_on_logging_change                   = $::confluent::params::connect_distributed_restart_on_logging_change,
+  Integer $config_storage_topic_replication_factor     = $::confluent::params::connect_distributed_config_storage_topic_replication_factor,
+  String $config_storage_topic_name                    = $::confluent::params::connect_distributed_config_storage_topic_name,
+  Integer $offset_storage_topic_replication_factor     = $::confluent::params::connect_distributed_offset_storage_topic_replication_factor,
+  String $offset_storage_topic_name                    = $::confluent::params::connect_distributed_offset_storage_topic_name,
+  Integer $status_storage_topic_replication_factor     = $::confluent::params::connect_distributed_status_storage_topic_replication_factor,
+  String $status_storage_topic_name                    = $::confluent::params::connect_distributed_status_storage_topic_name,
+  String $group_id                                     = $::confluent::params::connect_distributed_group_id,
+  Array[Stdlib::Unixpath] $plugin_path                 = $::confluent::params::connect_distributed_plugin_path,
+  String $key_converter                                = $::confluent::params::connect_distributed_key_converter,
+  String $value_converter                              = $::confluent::params::connect_distributed_value_converter,
+  Variant[String, Array[String]] $schema_registry_urls = ['http://localhost:8081/']
 ) inherits ::confluent::params {
   include ::confluent
   include ::confluent::kafka::connect
@@ -111,8 +122,36 @@ class confluent::kafka::connect::distributed (
     config => $actual_environment_settings
   }
 
+  if($key_converter == 'io.confluent.connect.avro.AvroConverter' and
+    !has_key($config, 'key.converter.schema.registry.url')) {
+    fail('key.converter.schema.registry.url must be defined in $config' )
+  }
+
+  if($value_converter == 'io.confluent.connect.avro.AvroConverter' and
+    !has_key($config, 'value.converter.schema.registry.url')) {
+      fail('value.converter.schema.registry.url must be defined in $config' )
+  }
+
   $default_config = {
-    'bootstrap.servers' => join(any2array($bootstrap_servers), ',')
+    'bootstrap.servers'                       => join(any2array($bootstrap_servers), ','),
+    'config.storage.replication.factor'       => $config_storage_topic_replication_factor,
+    'config.storage.topic'                    => $config_storage_topic_name,
+    'status.storage.replication.factor'       => $status_storage_topic_replication_factor,
+    'status.storage.topic'                    => $status_storage_topic_name,
+    'offset.storage.replication.factor'       => $offset_storage_topic_replication_factor,
+    'offset.storage.topic'                    => $offset_storage_topic_name,
+    'internal.key.converter.schemas.enable '  => false,
+    'internal.key.converter'                  => 'org.apache.kafka.connect.json.JsonConverter',
+    'internal.value.converter.schemas.enable' => false,
+    'internal.value.converter'                => 'org.apache.kafka.connect.json.JsonConverter',
+    'plugin.path'                             => join($plugin_path, ','),
+    'group.id'                                => $group_id,
+    'key.converter'                           => $key_converter,
+    'key.converter.schema.registry.url'     => join(any2array($schema_registry_urls), ','),
+    'key.converter.schemas.enable'            => false,
+    'value.converter'                         => $value_converter,
+    'value.converter.schema.registry.url'     => join(any2array($schema_registry_urls), ','),
+    'value.converter.schemas.enable'          => false
   }
   $actual_config = merge($default_config, $config)
   confluent::properties { $service_name:
